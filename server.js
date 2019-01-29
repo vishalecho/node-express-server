d953be390071d98a4f72dbcc603de9ea590969cb
vishalecho <vkr1994@gmail.com>
2019-01-30 00:37:17 +0530

    Initial commit.

---
+++ server.js
@@ -0,0 +1,58 @@
+var express = require("express");
+
+var app = express();
+
+app.use(express.static(__dirname + "/public")); //use static files in ROOT/public folder
+
+app.get("/defaultDishList", function (request, response) {
+    let itemList = {
+        "storeId": 3524,
+        "dishList": [
+            {
+                "dishId": "103",
+                "dishName": "Biryani",
+                "dishBatchNumber": "11230002384",
+                "dishDescription": "Taste of India's best biryani dum.",
+                "dishImage": "",
+                "dishCategory": "Non-Veg",
+                "sellingPrice": "180.00",
+                "tag": "Best Seller",
+                "rating": "4.3/5"
+            },
+            {
+                "dishId": "104",
+                "dishName": "Coffee",
+                "dishBatchNumber": "1123424384",
+                "dishDescription": "Best Coffee",
+                "dishImage": "Link or base64 encoded ? ",
+                "dishCategory": "Veg Beverage",
+                "sellingPrice": "100.00",
+                "tag": "",
+                "rating": "4.8/5"
+            }
+        ]
+    };
+    response.send(itemList);
+});
+
+app.get("/dishList", function (request, response) {
+    let itemList =
+    {
+        "storeId": 3524,
+        "dishList": [
+            {
+                "dishId": "103",
+                "discount": "20",
+                "position": 1
+            },
+            {
+                "dishId": "104",
+                "discount": "10",
+                "position": 2
+            }
+        ]
+    };
+    response.send(itemList);
+});
+
+app.listen(port, host);

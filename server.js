1417599c02d4751f851f3778e811c680873cf8a1
vishalecho <vkr1994@gmail.com>
2019-02-02 00:19:59 +0530

    File upload api created.

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 var express = require("express");
+var multer = require('multer');
+var bodyParser = require('body-parser')
 
 var app = express();
 const port=process.env.PORT || 3000;
@@ -98,6 +100,27 @@ app.get("/dishList", function (request, response) {
     };
     response.send(itemList);
 });
+
+var Storage = multer.diskStorage({
+    destination: function (req, file, callback) {
+        callback(null, "./Upload/Images");
+    },
+    filename: function (req, file, callback) {
+    callback(null, file.fieldname + "_" + Date.now() + "_" + file.originalname);
+    }
+});
+
+var upload = multer({ storage: Storage }).array("imgUploader", 3); //Field name and max count
+
+app.post("/api/Upload", function (req, res) {
+    upload(req, res, function (err) {
+    if (err) {
+    return res.end("Something went wrong!");
+    }
+    return res.end("File uploaded sucessfully!.");
+    });
+});
+
 
 app.listen(port,() => {
 
